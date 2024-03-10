b4cf5c7727d238a9ba03b45fbf8a058bdaad3b98
webtactics <paul@webtactics.com.au>
2024-03-10 17:02:29 +1100

    2

--- _includes/assets/js/script.js
+++ _includes/assets/js/script.js
@@ -84,11 +84,4 @@
 
 
 
-  const paralx2 = document.getElementById("paralx2");
-  
-  wrapper.onscroll = function () {
-    let y = wrapper.scrollTop;
-    paralx0.style.top = 1800 - y * 2.65 + "px";
-    paralx1.style.top = 1200 - y * 0.75 + "px";
-    paralx2.style.top = 800 + y * 0.5 + "px";
-  };
+
